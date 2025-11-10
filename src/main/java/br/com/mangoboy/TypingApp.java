8ced5bdedbd5214efc4b302c8f6772340cfb2cd2
Marco Mangan <marco.mangan@gmail.com>
2025-11-10 18:43:23 -0300

    First keyboard

--- src/main/java/br/com/mangoboy/TypingApp.java
+++ src/main/java/br/com/mangoboy/TypingApp.java
@@ -2,7 +2,9 @@ package br.com.mangoboy;
 
 import java.util.logging.Logger;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * 
@@ -16,15 +18,29 @@ public class TypingApp {
      */
     private static void createAndShowGUI() {
         final JFrame frame = new JFrame("Touch Typing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
-        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        String keys = """
+                QWERTYUIOP'[
+                ASDFGHJKLÇ~]
+                \\ZXCVBNM,.;/
+                """;
+        JPanel p = new JPanel();
+        for (char c : keys.toCharArray()) {
+            if (c != '\n') {
+                JButton b = new JButton("" + c);
+                p.add(b);
+            }
+        }
+        p.setOpaque(true);
+        frame.add(p);
         frame.pack();
-        frame.setVisible(true);        
+        frame.setVisible(true);
+
     }
 
     /**
      * 
-     * @param args
+     * @param args not used
      */
     public static void main(String[] args) {
         logger.info("Touch Typing!");
