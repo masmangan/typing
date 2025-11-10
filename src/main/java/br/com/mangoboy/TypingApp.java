c1331fb7fc6fac2f79c4686fc8824b0b26ac2622
Marco Mangan <marco.mangan@gmail.com>
2025-11-10 18:30:54 -0300

    First frame

--- src/main/java/br/com/mangoboy/TypingApp.java
+++ src/main/java/br/com/mangoboy/TypingApp.java
@@ -2,18 +2,33 @@ package br.com.mangoboy;
 
 import java.util.logging.Logger;
 
+import javax.swing.JFrame;
+
 /**
  * 
  */
 public class TypingApp {
 
     private static Logger logger = Logger.getLogger(TypingApp.class.getName());
-    
+
+    /**
+     * 
+     */
+    private static void createAndShowGUI() {
+        final JFrame frame = new JFrame("Touch Typing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+        
+        frame.pack();
+        frame.setVisible(true);        
+    }
+
     /**
      * 
      * @param args
      */
     public static void main(String[] args) {
         logger.info("Touch Typing!");
+        javax.swing.SwingUtilities.invokeLater(TypingApp::createAndShowGUI);
+
     }
 }
