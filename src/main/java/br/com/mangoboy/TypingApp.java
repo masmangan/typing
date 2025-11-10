bcbd6d9d83737b6e53e09da34f0cdb15b5bc78b7
Marco Mangan <marco.mangan@gmail.com>
2025-11-10 18:22:46 -0300

    First App

---
+++ src/main/java/br/com/mangoboy/TypingApp.java
@@ -0,0 +1,19 @@
+package br.com.mangoboy;
+
+import java.util.logging.Logger;
+
+/**
+ * 
+ */
+public class TypingApp {
+
+    private static Logger logger = Logger.getLogger(TypingApp.class.getName());
+    
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        logger.info("Touch Typing!");
+    }
+}
